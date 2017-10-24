420afe886fe37891820a0da8ffeb7aebfdfd079c
byrdie <roytsmart@gmail.com>
2017-10-24 00:07:49 -0600

    Got GPU and CPU versions working. So far CPU is faster :(

--- FinalProject/heat_parabolic_cuda/heat_parabolic_cuda.h
+++ FinalProject/heat_parabolic_cuda/heat_parabolic_cuda.h
@@ -9,6 +9,7 @@
 #define HEAT_PARABOLIC_CUDA_H_
 
 #include <stdio.h>
+#include <sys/time.h>
 #include <assert.h>
 #include <cuda.h>
 #include "common.h"
@@ -18,7 +19,7 @@
 void print_device_properties(cudaDeviceProp prop);
 
 __global__ void solve(float * T, float * t, float * x, float * y, float * z, uint Lt, uint Lx, uint Ly, uint Lz);
-__global__ void heat_1D(float * T, uint n, float dt, float * x, uint Lx, uint m_f, uint m_b);
+//__global__ void heat_1D(float * T, uint n, float dt, float * x, uint Lx, uint m_f, uint m_b);
 __device__ float D_F_1D(float A0, float A1, float s0, float s1);
 __device__ float load_T(float * T, int n, int i, int j, int k, uint Lx, uint Ly, uint Lz);
 __device__ float load_t(float * t, int n);
